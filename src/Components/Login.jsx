b68e7a5903f564ca472c3e7c4d4fbdb7511bb977
MTL1017 <ravitejaganji49@gmail.com>
2024-11-28 11:20:07 +0530

    login  change

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -126,7 +126,7 @@ const Login = () => {
             </main>
             <div className="welcome-container">
                 <h1 className="heading-secondary">
-                    Welcome to <span className="lg">Middleware Ravi!</span>
+                    Welcome to <span className="lg">Middleware Ravi Teja!</span>
                 </h1>
             </div>
         </div>
